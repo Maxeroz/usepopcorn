43e3d4d50b9d5d69c913b98b5cc7d1e0520484f0
Maxim <110911222+Maxeroz@users.noreply.github.com>
2024-03-03 21:37:34 +0400

    Creating our First Custom Hook: useMovies

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { toHaveErrorMessage } from "@testing-library/jest-dom/dist/matchers";
 import { useEffect, useRef, useState } from "react";
 import StarRating from "./StarRating";
+import { useMovies } from "./useMovies";
 
 const average = (arr) =>
   arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
@@ -9,10 +10,8 @@ const KEY = "6e788c35";
 
 export default function App() {
   const [query, setQuery] = useState("");
-  const [movies, setMovies] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState("");
   const [selectedId, setSelectedId] = useState(null);
+  const { movies, isLoading, error } = useMovies(query, handleCloseMovie);
 
   // const [watched, setWatched] = useState([]);
   const [watched, setWatched] = useState(function () {
@@ -38,33 +37,33 @@ export default function App() {
     setWatched((watched) => watched.filter((movie) => movie.imdbID !== id));
   }
 
-  const controller = new AbortController();
+  // const controller = new AbortController();
 
-  async function fetchMovies() {
-    try {
-      setIsLoading(true);
-      setError("");
-      const res = await fetch(
-        `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
-        { signal: controller.signal }
-      );
+  // async function fetchMovies() {
+  //   try {
+  //     setIsLoading(true);
+  //     setError("");
+  //     const res = await fetch(
+  //       `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
+  //       { signal: controller.signal }
+  //     );
 
-      if (!res.ok) throw new Error("Something went wrong with fetching movies");
+  //     if (!res.ok) throw new Error("Something went wrong with fetching movies");
 
-      const data = await res.json();
+  //     const data = await res.json();
 
-      if (data.Response === "False") throw new Error("Movie not found");
+  //     if (data.Response === "False") throw new Error("Movie not found");
 
-      setMovies(data.Search);
-      setError("");
-    } catch (err) {
-      if (err.name !== "AbortError") {
-        setError(err.message);
-      }
-    } finally {
-      setIsLoading(false);
-    }
-  }
+  //     setMovies(data.Search);
+  //     setError("");
+  //   } catch (err) {
+  //     if (err.name !== "AbortError") {
+  //       setError(err.message);
+  //     }
+  //   } finally {
+  //     setIsLoading(false);
+  //   }
+  // }
 
   useEffect(
     function () {
@@ -72,21 +71,6 @@ export default function App() {
     },
     [watched]
   );
-
-  useEffect(() => {
-    if (query.length < 3) {
-      setMovies([]);
-      setError("");
-      return;
-    }
-
-    handleCloseMovie();
-    fetchMovies();
-
-    return function () {
-      controller.abort();
-    };
-  }, [query]);
 
   return (
     <>
